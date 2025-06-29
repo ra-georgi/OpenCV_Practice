383ed6ba7e8a0d7e22df118a2c293a92a453f2b5
Reuben Georgi <reuben.abraham97@gmail.com>
2025-06-29 16:40:31 +0000

    Initial Commit

---
+++ 1_Basics/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/reuben/OpenCV_Practice/1_Basics/results/bin/main"
+  "/home/reuben/OpenCV_Practice/1_Basics/results/bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
