efc6daf1edaf4505f859d45961c89b5ec6a7ee67
Reuben Georgi <reuben.abraham97@gmail.com>
2026-02-16 05:45:15 +0000

    Binary search corrections

--- DSA_Reference/src/binary_search_5.cpp
+++ DSA_Reference/src/binary_search_5.cpp
@@ -1,22 +1,31 @@
 #include "binary_search_5.hpp"
 
-void binary_search(int target, std::vector<int> a)
+void binary_search(int target, const std::vector<int>& a)
 {
     int n = a.size();
     int left_idx  = 0;
+    // Possible bug here as std::vector::size() returns size_t (unsigned) and
+    // Because indexing uses size_t, and mixing signed/unsigned can silently convert -1 to a huge positive number in some refactors or future edits.
     int right_idx = n-1;
     int middle{0};
 
-    while (right_idx>left_idx)
+    while (right_idx>=left_idx)
     {
         middle = left_idx + ( (right_idx-left_idx)/2 );
-        if (middle = target)
+        if (a[middle] == target)
         {
             std::cout << "Target found \n";
             return;
         }
-        if (middle>target) right_idx = middle - 1;
-        if (middle<target) left_idx  = middle + 1;
+        if (a[middle]>target) 
+        {
+            right_idx = middle - 1;
+        }
+        else
+        {
+            left_idx  = middle + 1;
+        }
+        // if (a[middle]<target) left_idx  = middle + 1;
     }
 
     std::cout << "Target not present \n";
